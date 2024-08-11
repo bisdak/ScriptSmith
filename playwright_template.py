87ab9cc9a8928c7bf4dd2ccaefba78589d68733f
Kristian Marlowe Ole <krismar.ole@gmail.com>
2024-08-11 14:27:18 +0800

    playwright version

--- playwright_template.py
+++ playwright_template.py
@@ -1,16 +1,42 @@
 from string import Template
 
 script_template = Template('''
-from playwright.sync_api import sync_playwright
+    import asyncio
+    import urllib
+    import math
+    import re
+    import json
+    from typing import Any
 
-def run(playwright):
-    browser = playwright.$browser_type.launch(headless=$headless)
-    page = browser.new_page()
-    page.goto("$url")
-    page.screenshot(path="$screenshot_path")
-    browser.close()
+    from playwright.async_api import Page
+    
+    from harambe import SDK
+    from harambe import PlaywrightUtils as Pu
 
-with sync_playwright() as playwright:
-    run(playwright)
+    @SDK.scraper(domain="$domain", stage="$stage")
+    async def scrape(sdk: SDK, current_url: str, context: dict, *args: Any, **kwargs: Any) -> None:
+        page: Page = sdk.page
+        await page.wait_for_selector("$page_loaded_indicator")
+
+        $loop_content
+
+        await sdk.save_data({
+            'title': title,
+            'sku': sku if sku else None,
+            'description': description if description else None,
+            'type': type if type else None,
+            'price': final_price,
+            'images': images,
+            'features': features,
+            'attributes': attributes,
+            'interchange': [],
+            'fitment': fitment
+        })
+        
+    if __name__ == "__main__":
+        asyncio.run(
+            SDK.run(
+                scrape,"$url", headless=$headless,schema=None
+            )
+        )
 ''')
-
